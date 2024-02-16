cdb54c63a755e960e6ee711eed6f1ed19e2a78eb
07-ankur <im4ankur@gmail.com>
2024-02-17 00:56:01 +0530

    Login UI

--- User_Login/client/src/App.jsx
+++ User_Login/client/src/App.jsx
@@ -1,14 +1,42 @@
 // import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import Username from "./components/Username";
+import Password from "./components/Password";
+import Recovery from "./components/Recovery";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
+import Reset from "./components/Reset";
+import PageNotFound from "./components/PageNotFound";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Root Route</div>,
+    element: <Username></Username>,
   },
   {
     path: "/register",
-    element: <div>Register Route</div>,
+    element: <Register></Register>,
+  },
+  {
+    path: "/password",
+    element: <Password></Password>,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery></Recovery>,
+  },
+  {
+    path: "/profile",
+    element: <Profile></Profile>,
+  },
+  {
+    path: "/reset",
+    element: <Reset></Reset>,
+  },
+  {
+    path: "*",
+    element: <PageNotFound></PageNotFound>,
   },
 ]);
 
